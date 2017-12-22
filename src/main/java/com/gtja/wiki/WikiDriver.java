d12b02ceb71a99473f17f2337dae9188e2f9fd3e
yixiao1874 <1499957726@qq.com>
2017-12-22 14:26:19 +0800

    增加基于hadoop分布式电影推荐

--- src/main/java/com/gtja/wiki/WikiDriver.java
+++ src/main/java/com/gtja/wiki/WikiDriver.java
@@ -6,9 +6,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.mahout.math.VarLongWritable;
 import org.apache.mahout.math.VectorWritable;
 
